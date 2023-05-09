ec38c8c0967da030c01a629e0b7cab08cd0b687d
Trista Gem <savanatong@gmail.com>
2023-05-08 21:23:45 -0700

    fix java compile version

--- gulimall-member/src/main/java/com/example/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/example/gulimall/member/GulimallMemberApplication.java
@@ -3,10 +3,12 @@ package com.example.gulimall.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.example.gulimall.member.dao")
-public class GulimallMemberApplication {
+public class  GulimallMemberApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GulimallMemberApplication.class, args);
