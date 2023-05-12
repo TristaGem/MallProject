118035e98158a9553c51e5b6ee727e0c1cf987c6
Trista Gem <savanatong@gmail.com>
2023-05-11 17:36:52 -0700

    add oss client to upload image

---
+++ gulimall-thirdparty/src/main/java/com/example/gulimall/thirdparty/controller/OssController.java
@@ -0,0 +1,88 @@
+package com.example.gulimall.thirdparty.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.example.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class OssController {
+
+    @Autowired
+    OSS ossClient;
+
+    @Value("${spring.cloud.alicloud.oss.endpoint")
+    private String endpoint;
+
+    @Value("${spring.cloud.alicloud.oss.bucket")
+    private String bucket;
+
+    @Value("${spring.cloud.alicloud.access-key")
+    private String accessId;
+
+
+
+
+    @RequestMapping("/oss/policy")
+    public R policy() {
+
+        System.out.printf("get oss policy request");
+        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        String dir = format + "/";
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        Map<String, String> respMap = new LinkedHashMap<String, String>();
+
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+
+//            JSONObject jasonCallback = new JSONObject();
+//            jasonCallback.put("callbackUrl", callbackUrl);
+//            jasonCallback.put("callbackBody",
+//                    "filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}");
+//            jasonCallback.put("callbackBodyType", "application/x-www-form-urlencoded");
+//            String base64CallbackBody = BinaryUtil.toBase64String(jasonCallback.toString().getBytes());
+//            respMap.put("callback", base64CallbackBody);
+
+//            JSONObject ja1 = JSONObject.fromObject(respMap);
+//            // System.out.println(ja1.toString());
+//            response.setHeader("Access-Control-Allow-Origin", "*");
+//            response.setHeader("Access-Control-Allow-Methods", "GET, POST");
+//            response(request, response, ja1.toString());
+
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        }
+        return R.ok().put("data", respMap);
+    }
+}
